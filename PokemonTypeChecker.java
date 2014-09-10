a39265123d3caa3188a7aa1b08b690f72291ddcd
Luigi Vincent <luigivincenthr@gmail.com>
2014-09-10 15:01:54 -0400

    Update PokemonTypeChecker.java

--- PokemonTypeChecker.java
+++ PokemonTypeChecker.java
@@ -1,11 +1,7 @@
 /* Author: Luigi Vincent
-*A pokemon Type Evaluator, shows the user the pokemon's outstanding type attributes.
-*/
+Created on 9/8/2014
+ */
 
-import java.awt.BorderLayout;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.GridLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -13,220 +9,166 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
-
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class PokemonTypeChecker {
-	static Double normMulti1 = 1.0, flyMulti1 = 1.0, fightMulti1 = 1.0, fireMulti1 = 1.0,
-		waterMulti1 = 1.0, elecMulti1 = 1.0, grassMulti1 = 1.0, bugMulti1 = 1.0,
-		poisMulti1 = 1.0, darkMulti1 = 1.0, psyMulti1 = 1.0, ghostMulti1 = 1.0,
-		groundMulti1 = 1.0, rockMulti1 = 1.0, steelMulti1 = 1.0, iceMulti1 = 1.0,
-		dragMulti1 = 1.0, faeMulti1 = 1.0;
 
-    static String[] elements = {"Normal", "Flying", "Fighting", "Fire", "Water",
-    	"Electric", "Grass", "Bug", "Poison", "Dark", "Psychic", "Ghost",
-       	"Ground", "Rock", "Steel", "Ice", "Dragon", "Fairy"};
+    static PokemonType normal = new PokemonType.Builder(Element.NORMAL)
+    .vulnerableTo(Element.FIGHTING).immuneTo(Element.GHOST).generate();
 
-    static int len = elements.length;
-    static Double[] multiplierList1 = new Double[len];
+    static PokemonType fire = new PokemonType.Builder(Element.FIRE)
+    .vulnerableTo(Element.WATER, Element.GROUND, Element.ROCK)
+    .resistantTo(Element.FIRE, Element.GRASS, Element.ICE,
+        Element.BUG, Element.STEEL, Element.FAIRY)
+    .generate();
 
-    static JTextField immune = new JTextField(15), res = new JTextField(15),
-    	vuln = new JTextField(15);
+    static PokemonType water = new PokemonType.Builder(Element.WATER)
+    .vulnerableTo(Element.ELECTRIC, Element.GRASS)
+    .resistantTo(Element.FIRE, Element.WATER, Element.ICE, Element.STEEL)
+    .generate();
+
+    static PokemonType electric = new PokemonType.Builder(Element.ELECTRIC)
+    .resistantTo(Element.ELECTRIC, Element.STEEL, Element.FLYING)
+    .vulnerableTo(Element.GROUND).generate();
+
+    static PokemonType grass = new PokemonType.Builder(Element.GRASS)
+    .resistantTo(Element.GRASS, Element.WATER, Element.GROUND, Element.ELECTRIC)
+    .vulnerableTo(Element.FIRE, Element.ICE,
+        Element.POISON, Element.FLYING, Element.BUG)
+    .generate();
+
+    static PokemonType ice = new PokemonType.Builder(Element.ICE)
+    .vulnerableTo(Element.FIRE, Element.STEEL, Element.ROCK, Element.FIGHTING)
+    .resistantTo(Element.ICE).generate();
+
+    static PokemonType fighting = new PokemonType.Builder(Element.FIGHTING)
+    .vulnerableTo(Element.PSYCHIC, Element.FLYING, Element.FAIRY)
+    .resistantTo(Element.BUG, Element.ROCK, Element.DARK)
+    .generate();
+
+    static PokemonType poison = new PokemonType.Builder(Element.POISON)
+    .resistantTo(Element.GRASS, Element.FIGHTING, Element.POISON,
+        Element.BUG, Element.FAIRY)
+    .vulnerableTo(Element.GROUND, Element.PSYCHIC).generate();
+
+    static PokemonType ground = new PokemonType.Builder(Element.GROUND)
+    .resistantTo(Element.POISON, Element.ROCK)
+    .vulnerableTo(Element.WATER, Element.GRASS, Element.ICE)
+    .immuneTo(Element.ELECTRIC).generate();
+
+    static PokemonType flying = new PokemonType.Builder(Element.FLYING)
+    .resistantTo(Element.GRASS, Element.FIGHTING, Element.BUG)
+    .vulnerableTo(Element.ELECTRIC, Element.ICE, Element.ROCK)
+    .immuneTo(Element.GROUND).generate();
+
+    static PokemonType psychic = new PokemonType.Builder(Element.PSYCHIC)
+    .vulnerableTo(Element.BUG, Element.DARK, Element.GHOST)
+    .resistantTo(Element.FIGHTING, Element.PSYCHIC).generate();
+
+    static PokemonType bug = new PokemonType.Builder(Element.BUG)
+    .vulnerableTo(Element.FIRE, Element.FLYING, Element.ROCK)
+    .resistantTo(Element.GRASS, Element.FIGHTING, Element.GROUND)
+    .generate();
+
+    static PokemonType rock = new PokemonType.Builder(Element.ROCK)
+    .vulnerableTo(Element.WATER, Element.GRASS, Element.FIGHTING,
+        Element.GROUND, Element.STEEL)
+    .resistantTo(Element.NORMAL, Element.FIRE, Element.POISON, Element.FLYING)
+    .generate();
+
+    static PokemonType ghost = new PokemonType.Builder(Element.GHOST)
+    .vulnerableTo(Element.GHOST, Element.DARK)
+    .resistantTo(Element.BUG, Element.POISON)
+    .immuneTo(Element.NORMAL, Element.FIGHTING)
+    .generate();
+
+    static PokemonType dragon = new PokemonType.Builder(Element.DRAGON)
+    .vulnerableTo(Element.ICE, Element.DRAGON, Element.FAIRY)
+    .resistantTo(Element.WATER, Element.FIRE, Element.ELECTRIC, Element.GRASS)
+    .generate();
+
+    static PokemonType dark = new PokemonType.Builder(Element.DARK)
+    .vulnerableTo(Element.FIGHTING, Element.BUG, Element.FAIRY)
+    .resistantTo(Element.GHOST, Element.DARK)
+    .immuneTo(Element.PSYCHIC).generate();
+
+    static PokemonType steel = new PokemonType.Builder(Element.STEEL)
+    .vulnerableTo(Element.FIRE, Element.FIGHTING, Element.GROUND)
+    .resistantTo(Element.NORMAL, Element.GRASS, Element.ICE, Element.FLYING,
+        Element.PSYCHIC, Element.BUG, Element.ROCK, Element.DRAGON,
+        Element.STEEL, Element.FAIRY)
+    .immuneTo(Element.POISON).generate();
+
+    static PokemonType fairy = new PokemonType.Builder(Element.FAIRY)
+    .vulnerableTo(Element.STEEL, Element.POISON)
+    .resistantTo(Element.FIGHTING, Element.DARK, Element.BUG)
+    .immuneTo(Element.DRAGON).generate();
+
+    static PokemonType none = new PokemonType.Builder(Element.NONE).generate();
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Type Checker");
-        JPanel panel = new JPanel(), result = new JPanel();
+        JFrame frame = new JFrame("Pokemon Type Checker");
+        JPanel selectionPanel = new JPanel(), result = new JPanel();
         result.setLayout(new GridLayout(3, 2, 0, 2));
+
+        JTextField immunities = new JTextField(15), resistances = new JTextField(15),
+        vulnerabilities = new JTextField(15);
+        immunities.setEditable(false);
+        resistances.setEditable(false);
+        vulnerabilities.setEditable(false);
+
         result.add(new JLabel("Immune to:"));
-        result.add(immune);
+        result.add(immunities);
         result.add(new JLabel("Resistant to:"));
-        result.add(res);
+        result.add(resistances);
         result.add(new JLabel("Vulnerable to:"));
-        result.add(vuln);
-        
-        vuln.setEditable(false);
-        res.setEditable(false);
-        immune.setEditable(false);
+        result.add(vulnerabilities);
 
-        multiplerSet();
+        JComboBox<ImageIcon> typeList = new JComboBox<ImageIcon>();
+        JComboBox<ImageIcon> typeList2 = new JComboBox<ImageIcon>();
 
-        ImageIcon[] types = new ImageIcon[len];
-        for (int i = 0; i < len; i++) {
-        	types[i] = new ImageIcon("Images/" + elements[i] + ".gif");
+        for (Element e : Element.values()) {
+            typeList.addItem(e.icon);
+            typeList2.addItem(e.icon);
         }
 
-        JComboBox<ImageIcon> typeList = new JComboBox<ImageIcon>(types);
-        typeList.addItemListener(new ItemListener() {
-     		@Override
-     		public void itemStateChanged(ItemEvent e) {
-     			if (e.getStateChange() == ItemEvent.SELECTED) {
-     				multiplierReset();
-       				multiplierDisplay();
+        ItemListener typeListener = e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                if (e.getSource() == typeList) {
+                    displayAttributes(fire, none);
+                }
+                else if (e.getSource() == typeList2) {
+                    System.out.println("Source 2: " + e.toString());
+                }
+            }
+        };
+        
+        typeList.addItemListener(typeListener);
+        typeList2.addItemListener(typeListener);
 
-     			 	for (int i = 0; i < len; i++) {
-     			 		if (e.getItem() == types[i]) {
-     			 			switch(elements[i]) {
-     			 				case "Normal":
-     			 					fightMulti1 = 2.0;
-     			 					ghostMulti1 = 0.0;
-     			 					break;
-     			 				case "Flying":
-     			 					elecMulti1 = 2.0; iceMulti1 = 2.0; rockMulti1 = 2.0;
-     			 					bugMulti1 = 0.5; fightMulti1 = 0.5; grassMulti1 = 0.5;
-     			 					groundMulti1 = 0.0;
-     			 					break;
-     			 				case "Fighting":
-     			 					flyMulti1 = 2.0; psyMulti1 = 2.0;
-     			 					bugMulti1 = 0.5; rockMulti1 = 0.5;
-     			 					break;
-     			 				case "Fire":
-     			 					groundMulti1 = 2.0; rockMulti1 = 2.0; waterMulti1 = 2.0;
-     			 					bugMulti1 = 0.5; faeMulti1 = 0.5; fireMulti1 = 0.5;
-     			 					grassMulti1 = 0.5; iceMulti1 = 0.5; steelMulti1 = 0.5;
-     			 					break;
-     			 				case "Water":
-     			 					elecMulti1 = 2.0; grassMulti1 = 2.0;
-     			 					fireMulti1 = 0.5; iceMulti1 = 0.5;
-     			 					steelMulti1 = 0.5; waterMulti1 = 0.5;
-     			 					break;
-     			 				case "Electric":
-     			 					groundMulti1 = 2.0;
-     			 					elecMulti1 = 0.5; flyMulti1 = 0.5; steelMulti1 = 0.5;
-     			 					break;
-     			 				case "Grass":
-     			 					bugMulti1 = 2.0; iceMulti1 = 2.0;
-     			 					fireMulti1 = 2.0; flyMulti1 = 2.0; poisMulti1 = 2.0;
-     			 					elecMulti1 = 0.5; grassMulti1 = 0.5;
-     			 					groundMulti1 = 0.5; waterMulti1 = 0.5;
-     			 					break;
-     			 				case "Bug":
-     			 					fireMulti1 = 2.0; flyMulti1 = 2.0; rockMulti1 = 2.0;
-     			 					fightMulti1 = 0.5; grassMulti1 = 0.5; groundMulti1 = 0.5;
-     			 					break;
-     			 				case "Poison":
-     			 					groundMulti1 = 2.0; psyMulti1 = 2.0;
-     			 					bugMulti1 = 0.5; faeMulti1 = 0.5;
-     			 					fightMulti1 = 0.5; grassMulti1 = 0.5; poisMulti1 = 0.5;
-     			 					break;
-     			 				case "Dark":
-     			 					bugMulti1 = 2.0; faeMulti1 = 2.0; fightMulti1 = 2.0;
-     			 					darkMulti1 = 0.5; ghostMulti1 = 0.5;
-     			 					psyMulti1 = 0.0;
-     			 					break;
-     			 				case "Psychic":
-     			 					bugMulti1 = 2.0; darkMulti1 = 2.0; ghostMulti1 = 2.0;
-     			 					fightMulti1 = 0.5; psyMulti1 = 0.5;
-     			 					break;
-     			 				case "Ghost":
-     			 					darkMulti1 = 2.0; ghostMulti1 = 2.0;
-     			 					bugMulti1 = 0.5; poisMulti1 = 0.5;
-     			 					fightMulti1 = 0.0; normMulti1 = 0.0;
-     			 					break;
-     			 				case "Ground":
-     			 					iceMulti1 = 2.0; grassMulti1 = 2.0; waterMulti1 = 2.0;
-     			 					poisMulti1 = 0.5; rockMulti1 = 0.5;
-     			 					elecMulti1 = 0.0;
-     			 					break;
-     			 				case "Rock":
-     			 					fightMulti1 = 2.0; grassMulti1 = 2.0;
-     			 					groundMulti1 = 2.0; steelMulti1 = 2.0; waterMulti1 = 2.0;
-     			 					fireMulti1 = 0.5; flyMulti1 = 0.5;
-     			 					normMulti1 = 0.5; poisMulti1 = 0.5;
-     			 					break;
-     			 				case "Steel":
-     			 					fightMulti1 = 2.0; fireMulti1 = 2.0; groundMulti1 = 2.0;
-     			 					bugMulti1 = 0.5; dragMulti1 = 0.5; faeMulti1 = 0.5;
-     			 					flyMulti1 = 0.5; grassMulti1 = 0.5; iceMulti1 = 0.5;
-     			 					normMulti1 = 0.5; psyMulti1 = 0.5;
-     			 					rockMulti1 = 0.5; steelMulti1 = 0.5;
-     			 					poisMulti1 = 0.0;
-     			 					break;
-     			 				case "Ice":
-     			 					fightMulti1 = 2.0; fireMulti1 = 2.0;
-     			 					rockMulti1 = 2.0; steelMulti1 = 2.0;
-     			 					iceMulti1 = 0.5;
-     			 					break;
-     			 				case "Dragon":
-     			 					dragMulti1 = 2.0; faeMulti1 = 2.0; iceMulti1 = 2.0;
-     			 					elecMulti1 = 0.5; fireMulti1 = 0.5;
-     			 					grassMulti1 = 0.5; waterMulti1 = 0.5;
-     			 					break;
-     			 				case "Fairy":
-     			 					poisMulti1 = 2.0; steelMulti1 = 2.0;
-     			 					bugMulti1 = 0.5; darkMulti1 = 0.5; fightMulti1 = 0.5;
-     			 					dragMulti1 = 0.0;
-     			 				break; // final break purposely indented such
-     			 			}
-     			 			multiplerSet();
-     			 			multiplierDisplay();
-     			 		}
-     			 	}
-       			}
-     		}
- 		});
+        selectionPanel.add(typeList);
+        selectionPanel.add(typeList2);
 
-        panel.add(typeList);
-
-        frame.setIconImage(new ImageIcon("Images/Icon.png").getImage());
         frame.add(result, BorderLayout.CENTER);
-        frame.add(panel, BorderLayout.SOUTH);
-        frame.pack();
+        frame.add(selectionPanel, BorderLayout.SOUTH);
+        frame.setIconImage(new ImageIcon("Images/Icon.png").getImage());
 
+        frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
-    public static void multiplerSet() {
-    	multiplierList1[0] = normMulti1;
-	    multiplierList1[1] = flyMulti1;
-	    multiplierList1[2] = fightMulti1;
-	    multiplierList1[3] = fireMulti1;
-	    multiplierList1[4] = waterMulti1;
-	    multiplierList1[5] = elecMulti1;
-	    multiplierList1[6] = grassMulti1;
-	    multiplierList1[7] = bugMulti1;
-	    multiplierList1[8] = poisMulti1;
-	    multiplierList1[9] = darkMulti1;
-	    multiplierList1[10] = psyMulti1;
-	    multiplierList1[11] = ghostMulti1;
-	    multiplierList1[12] = groundMulti1;
-	    multiplierList1[13] = rockMulti1;
-	    multiplierList1[14] = steelMulti1;
-	    multiplierList1[15] = iceMulti1;
-	    multiplierList1[16] = dragMulti1;
-	    multiplierList1[17] = faeMulti1;
-    }
-
-    public static void multiplierReset() {
-    	for (int i = 0; i < len; i++) {
-    		multiplierList1[i] = 1.0;
-    	}
-    }
-
-    public static void multiplierDisplay() {
-    	StringBuilder vln = new StringBuilder(), resist = new StringBuilder(),
-    		imm = new StringBuilder();;
-
-    	for (int i = 0; i < len; i++) {
-    		if (multiplierList1[i] == 0.0) {
-    			if (imm.toString().isEmpty()) { imm.append(elements[i]); }
-    			else { imm.append(", " + elements[i]); }
-    		}
-    		else if (multiplierList1[i] == 0.5) {
-    			if (resist.toString().isEmpty()) { resist.append(elements[i]); }
-    			else { resist.append(", " + elements[i]); }
-    		}
-    		else if (multiplierList1[i] == 2) {
-    			if (vln.toString().isEmpty()) { vln.append(elements[i]); }
-    			else { vln.append(", " + elements[i]); }
-    		}
-    	}
-
-    	if (vln.toString().isEmpty()) { vln.append("None"); }
-	   	if (imm.toString().isEmpty()) { imm.append("None"); }
-	   	if (resist.toString().isEmpty()) { resist.append("None"); }
-	   	immune.setText(imm.toString());
-	   	res.setText(resist.toString());
-	   	vuln.setText(vln.toString());
+    public static void displayAttributes(PokemonType primary, PokemonType secondary) {
+        for(Map.Entry<Element, Double> entry : primary.getMultipliers().entrySet()){
+            System.out.println(entry.getKey() +" : "+ entry.getValue());
+        }
     }
 }
